0f09df42837509109fa7cb45175bb0d75002a4a3
MayarElhadedy <yarafawzy90@hotmail.com>
2024-05-26 22:24:19 +0300

    final

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const bar = document.getElementById("bar");
 const nav = document.getElementById("navbar");
 const close = document.getElementById("close");
+let iconcar = document.getElementById("cart-item");
 if (bar) {
   bar.addEventListener("click", () => {
     nav.classList.add("active");
@@ -11,3 +12,4 @@ if (close) {
     nav.classList.remove("active");
   });
 }
+cart - item.addEventListener("click", () => {});
